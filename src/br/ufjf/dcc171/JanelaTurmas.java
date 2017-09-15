21191bd7d30661a17155572072f8377257dbe86e
Igor Knop <igorknop@gmail.com>
2017-09-14 21:53:07 -0300

    Adiciona eventos de turma

--- src/br/ufjf/dcc171/JanelaTurmas.java
+++ src/br/ufjf/dcc171/JanelaTurmas.java
@@ -3,12 +3,16 @@ package br.ufjf.dcc171;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 import javax.swing.Box;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
@@ -16,7 +20,7 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 public class JanelaTurmas extends JFrame {
-    
+
     private final List<Turma> turmas;
     private final JList<Turma> lstTurmas = new JList<>(new DefaultListModel<>());
     private final JList<Aluno> lstAlunos = new JList<>(new DefaultListModel<>());
@@ -24,11 +28,10 @@ public class JanelaTurmas extends JFrame {
     private final JButton excluiTurma = new JButton("Exclui Turma");
     private final JButton criaAluno = new JButton("Cria Aluno");
     private final JButton excluiAluno = new JButton("Exclui Aluno");
-    
 
     public JanelaTurmas(List<Turma> sampleData) {
         super("Turmas");
-        setMinimumSize(new Dimension(500,300));
+        setMinimumSize(new Dimension(500, 300));
         this.turmas = sampleData;
         lstTurmas.setModel(new TurmasListModel(turmas));
         add(new JScrollPane(lstTurmas), BorderLayout.WEST);
@@ -40,7 +43,7 @@ public class JanelaTurmas extends JFrame {
         botoes.add(criaAluno);
         botoes.add(excluiAluno);
         add(botoes, BorderLayout.SOUTH);
-        
+
         lstTurmas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         lstTurmas.addListSelectionListener(new ListSelectionListener() {
             @Override
@@ -54,7 +57,30 @@ public class JanelaTurmas extends JFrame {
                 }
             }
         });
-        
+
+        criaTurma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String cod = JOptionPane.showInputDialog("Código da nova turma");
+                if (cod != null && !cod.isEmpty()) {
+                    Turma t = new Turma(cod);
+                    turmas.add(t);
+                    lstTurmas.updateUI();
+                }
+            }
+        });
+
+        excluiTurma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (lstTurmas.isSelectionEmpty()) {
+                    return;
+                }
+                turmas.remove(lstTurmas.getSelectedValue());
+                lstTurmas.clearSelection();
+                lstTurmas.updateUI();
+            }
+        });
     }
-    
+
 }
